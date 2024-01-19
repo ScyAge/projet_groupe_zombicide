ae7c6f6fd4285b6e7679ca13d307cf5607318576
Ab <Ab@10.245.207.22>
2024-01-19 14:11:24 +0100

    ajout des méthodes canLook et toString

---
+++ src/Cell/Continental.java
@@ -0,0 +1,21 @@
+package Cell;
+
+import Cell.Room;
+
+public class Continental extends Room {
+
+	public Continental(int x, int y, String type) {
+		super(x, y, type);
+	}
+	
+	@Override
+	public boolean canLook() {
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return "players are in a rest, there are no attacks";
+	}
+	
+}
