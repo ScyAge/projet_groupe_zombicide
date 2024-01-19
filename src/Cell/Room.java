7669064adc0a26f4d8b5478834afccd7956fd883
Ab <Ab@10.245.207.22>
2024-01-19 12:33:37 +0100

    ajout de la classe Room

---
+++ src/Cell/Room.java
@@ -0,0 +1,43 @@
+package Cell;
+import java.util.*;
+
+import Item.Item;
+
+public class Room extends Cell {
+	private List<Item> items;
+	
+	/** Constructor of the class Room
+	 * @param x coordinates of the cell
+	 * @param y coordinates of the cell
+	 * @param type the type of the cell 
+	 */
+	public Room(int x, int y, String type) {
+		super(x, y, type);
+		this.items = new ArrayList<>();
+	}
+	
+	/**
+	 * Return the list of all the items in the room
+	 * @return List of items
+	 */
+	public List<Item> getAllItems() {
+		return this.items;
+	}
+	
+	/**
+	 * adds an item to the list
+	 * @param  i Item,
+	 */
+	public void addItem(Item i) {
+		this.items.add(i) ;
+	}
+	
+	
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
