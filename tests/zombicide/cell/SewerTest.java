17d9eda3b11da051d8dc296a05788d57e2826f63
Théo Nave <theon@10.135.5.127>
2024-02-02 01:47:07 +0100

    update SewerTest

--- tests/zombicide/cell/SewerTest.java
+++ tests/zombicide/cell/SewerTest.java
@@ -22,50 +22,6 @@ class SewerTest {
     }
     
     @Test
-    void testAddRemovePlayer() {
-    	Player player1 = new Player();
-    	sewer.addPlayers(player);
-    	sewer.addPlayers(player1);
-    	
-    	assertEquals(2, sewer.getAllPlayers().size());
-    	assertTrue(sewer.getAllPlayers().contains(player));
-    	assertTrue(sewer.getAllPlayers().contains(player1));
-    	
-    	sewer.RemovePlayer(player1);
-    	assertEquals(1, sewer.getAllPlayers().size());
-    	assertFalse(sewer.getAllPlayers().contains(player1));
-    	
-    }
-    
-    @Test
-    void testAddRemoveZombie() {
-    	Zombies zombie1 = new Zombies();
-    	sewer.addZombies(zombie);
-    	sewer.addZombies(zombie1);
-    	
-    	assertEquals(2, sewer.getAllZombies().size());
-    	assertTrue(sewer.getAllZombies().contains(zombie));
-    	assertTrue(sewer.getAllZombies().contains(zombie1));
-    	
-    	sewer.RemoveZombie(zombie1);
-    	assertEquals(1, sewer.getAllZombies().size());
-    	assertFalse(sewer.getAllZombies().contains(zombie1));
-    	
-    }
-    
-    @Test
-    void testCoordinates() {
-    	Sewer sewer1 = new Sewer(4,6);
-    	assertEquals(4, sewer1.getX());
-    	assertEquals(6, sewer1.getY());
-    }
-    
-    @Test
-    void testCanLook() {
-    	assertTrue(sewer.canLook());
-    }
-    
-    @Test
     void testProductionZombie() {
     	assertEquals(0, sewer.getAllZombies().size());
     	
