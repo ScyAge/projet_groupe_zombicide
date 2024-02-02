4aeaba41e9a285dfd4c39f78f30565cbf316927d
Théo Nave <theon@10.135.5.127>
2024-02-02 01:53:43 +0100

    completed ContinentalTest

---
+++ tests/zombicide/cell/ContinentalTest.java
@@ -0,0 +1,35 @@
+package zombicide.cell;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import zombicide.actor.player.Player;
+import zombicide.actor.zombie.Zombies;
+import zombicide.item.Item;
+import zombicide.util.Door;
+
+class ContinentalTest {
+
+	private Continental continental;
+    private Player player;
+    private Zombies zombie;
+    private Item item;
+    private Door door;
+    
+    @BeforeEach
+    void setUp() {
+        continental = new Continental(1, 1);
+        player = new Player();
+        zombie = new Zombies();
+        item = new Item();
+        door = new Door(true);
+    }
+    
+    @Test
+    void testCanLook() {
+    	assertFalse(continental.canLook());
+    }
+
+}
