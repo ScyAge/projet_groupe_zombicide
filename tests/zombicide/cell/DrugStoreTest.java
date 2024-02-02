a1606c464475dc941259c7beeeba58ab4cdacac4
Théo Nave <theon@10.135.5.127>
2024-02-02 01:51:45 +0100

    completed DrugStoreTest

--- tests/zombicide/cell/DrugStoreTest.java
+++ tests/zombicide/cell/DrugStoreTest.java
@@ -1,5 +1,72 @@
 package zombicide.cell;
 
-public class DrugStoreTest {
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import zombicide.actor.player.Player;
+import zombicide.actor.zombie.Zombies;
+import zombicide.item.HealingVial;
+import zombicide.item.Item;
+import zombicide.util.Door;
+
+class DrugStoreTest {
+	
+	private DrugStore drugStore;
+    private Player player;
+    private Zombies zombie;
+    private Item item;
+    private Door door;
+    private HealingVial healingVial;
+    
+    @BeforeEach
+    void setUp() {
+    	drugStore = new DrugStore(1, 1);
+        player = new Player();
+        zombie = new Zombies();
+        item = new Item();
+        door = new Door(true);
+        healingVial = new HealingVial();
+    }
+    
+    @Test
+    void testAddAndGetHealingVial() {
+    	HealingVial healingVial2 = new HealingVial();
+    	
+    	drugStore.addHealingVial(healingVial);
+    	drugStore.addHealingVial(healingVial2);
+    	
+    	assertEquals(2, drugStore.getHealingVials().size());
+    	assertTrue(drugStore.getHealingVials().contains(healingVial));
+    	assertTrue(drugStore.getHealingVials().contains(healingVial2));
+    }
+
+    @Test
+    void testAddPlayer() {
+    	assertEquals(0, drugStore.getAllPlayers().size());
+        assertEquals(0, drugStore.getHealingVials().size());
+        
+        drugStore.addPlayers(player);
+        
+        assertEquals(1, drugStore.getAllPlayers().size());
+        assertEquals(1, drugStore.getHealingVials().size());
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
