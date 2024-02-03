c2f07e449eba45be95726176508e691f9099fa76
Joly <evan.joly.etu@univ-lille.fr>
2024-02-03 15:46:13 +0100

    supression d'attribut de Door et devient abstract

--- src/zombicide/util/Door.java
+++ src/zombicide/util/Door.java
@@ -2,27 +2,19 @@ package zombicide.util;
 
 import java.util.*;
 
-public class Door {
+public abstract class Door {
 
 	private boolean Breakable;
 	
 	private boolean Break; 
 	
-	private Direction direction_door;
-	private Map<Direction,String> direction_affichage;
 	/**
 	 * Door Builder
 	 * @param Breakable if a door is unbreakable or not 
 	 */
-	public Door(boolean Breakable,Direction direction_door) {
+	public Door(boolean Breakable) {
 		this.Break = false;
 		this.Breakable = Breakable;
-		this.direction_door = direction_door;
-		this.direction_affichage = new HashMap<>();
-		this.direction_affichage.put(Direction.North, "___");
-		this.direction_affichage.put(Direction.South, "___");
-		this.direction_affichage.put(Direction.East, "|\n|\n|");
-		this.direction_affichage.put(Direction.West, "|\n|\n|");
 	}
 	
 	/**
@@ -48,12 +40,9 @@ public class Door {
 	}
 
 	@Override
-	public String toString() {
-		if(this.isBreak()) {
-			return "";
-		}
-		return this.direction_affichage.get(this.direction_door);
-	}
+	public abstract String toString();
+		
+	
 	
 	
 } 
