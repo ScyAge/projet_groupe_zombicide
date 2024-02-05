d09667e58d1d7fb223715734611cf0706ffbdf70
joly <evan.joly.etu@univ-lille.fr>
2024-02-05 09:53:42 +0100

    test sewer

--- tests/zombicide/cell/SewerTest.java
+++ tests/zombicide/cell/SewerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import zombicide.actor.player.Player;
 import zombicide.actor.zombie.Zombies;
+import zombicide.util.Direction;
 
 class SewerTest {
 
@@ -28,6 +29,45 @@ class SewerTest {
     	sewer.ProductionZombie(3);
     	
     	assertEquals(3, sewer.getAllZombies().size());
+    }
+    
+    @Test
+    void testToStringLine1() {
+    	assertEquals(sewer.toString(0),"     ");
+    }
+    
+    
+
+    void testToStringLine2SomeBodyInCell() {
+    	sewer.addZombies(zombie);
+    	assertEquals(sewer.toString(1)," "+"\u001B[33m" + "E" + "\u001B[0m"+"z1 ");
+    }
+
+	
+    	
+    @Test
+    void testToStringLine2NoBodyInCell() {	
+    	assertEquals(sewer.toString(1)," "+"\u001B[33m" + "E" + "\u001B[0m"+"   ");
+    }
+    
+    
+    
+    void testToStringLine3SomeBodyInCell() {
+    	sewer.addZombies(zombie);
+    	assertEquals(sewer.toString(2)," "+"\u001B[33m" + "E" + "\u001B[0m"+"s1 ");
+
+    }
+    	
+    @Test
+    void testToStringLine3NoBodyInCell() {	
+    	assertEquals(sewer.toString(2),"     ");
+
+    
+    }
+    
+    @Test
+    void testToStringLine4() {
+    	assertEquals(sewer.toString(3),"     ");
     }
 
 }
