1c7480b0eb47716c74ff2cafc68851a363f3a74b
joly <evan.joly.etu@univ-lille.fr>
2024-02-05 09:35:37 +0100

    test DrugStore

--- tests/zombicide/cell/DrugStoreTest.java
+++ tests/zombicide/cell/DrugStoreTest.java
@@ -9,6 +9,7 @@ import zombicide.actor.player.Player;
 import zombicide.actor.zombie.Zombies;
 import zombicide.item.HealingVial;
 import zombicide.item.Item;
+import zombicide.util.Direction;
 import zombicide.util.Door;
 
 class DrugStoreTest {
@@ -49,6 +50,72 @@ class DrugStoreTest {
         
         assertEquals(1, drugStore.getAllPlayers().size());
         assertEquals(1, drugStore.getHealingVials().size());
+    }
+    
+    @Test
+    void testToStringLine1() {
+    	/*Door north not break*/
+    	assertEquals(drugStore.toString(0),"-----");
+    	/*Door north  break*/
+    	drugStore.getDoor(Direction.North).Break();
+    	assertEquals(drugStore.toString(0),"     ");
+    }
+    
+    
+
+    void testToStringLine2SomeBodyInCell() {
+    	drugStore.addZombies(zombie);
+    	/*Door north not break*/
+    	assertEquals(drugStore.toString(1),"|"+"\u001B[31m" +"D"+ "\u001B[0m"+"z1|");
+    	/*Door north  break*/
+    	drugStore.getDoor(Direction.East).Break();
+    	drugStore.getDoor(Direction.West).Break();
+    	assertEquals(drugStore.toString(1)," "+"\u001B[31m" +"D"+ "\u001B[0m"+"z1 ");
+    }
+
+	
+    	
+    @Test
+    void testToStringLine2NoBodyInCell() {	
+    	/*Door north not break*/
+    	assertEquals(drugStore.toString(1),"|"+"\u001B[31m" +"D"+ "\u001B[0m"+"  |");
+    	/*Door north  break*/
+    	drugStore.getDoor(Direction.East).Break();
+    	drugStore.getDoor(Direction.West).Break();
+    	assertEquals(drugStore.toString(1)," "+"\u001B[31m" +"D"+ "\u001B[0m"+"   ");
+    
+    }
+    
+    
+    
+    void testToStringLine3SomeBodyInCell() {
+    	drugStore.addZombies(zombie);
+    	/*Door north not break*/
+    	assertEquals(drugStore.toString(2),"|s1 |");
+    	/*Door north  break*/
+    	drugStore.getDoor(Direction.East).Break();
+    	drugStore.getDoor(Direction.West).Break();
+    	assertEquals(drugStore.toString(2)," s1  ");
+    }
+    	
+    @Test
+    void testToStringLine3NoBodyInCell() {	
+    	/*Door north not break*/
+    	assertEquals(drugStore.toString(2),"|   |");
+    	/*Door north  break*/
+    	drugStore.getDoor(Direction.East).Break();
+    	drugStore.getDoor(Direction.West).Break();
+    	assertEquals(drugStore.toString(2),"     ");
+    
+    }
+    
+    @Test
+    void testToStringLine4() {
+    	/*Door north not break*/
+    	assertEquals(drugStore.toString(3),"-----");
+    	/*Door north  break*/
+    	drugStore.getDoor(Direction.South).Break();
+    	assertEquals(drugStore.toString(3),"     ");
     }
 }
 
