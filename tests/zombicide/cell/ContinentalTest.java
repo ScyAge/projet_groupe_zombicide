2885a418759802143837efba18071d6dece9deb8
joly <evan.joly.etu@univ-lille.fr>
2024-02-05 09:31:33 +0100

    ajout test continental

--- tests/zombicide/cell/ContinentalTest.java
+++ tests/zombicide/cell/ContinentalTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import zombicide.actor.player.Player;
 import zombicide.actor.zombie.Zombies;
 import zombicide.item.Item;
+import zombicide.util.Direction;
 import zombicide.util.Door;
 
 class ContinentalTest {
@@ -16,6 +17,7 @@ class ContinentalTest {
     private Player player;
     private Zombies zombie;
     private Item item;
+    
     
     @BeforeEach
     void setUp() {
@@ -29,5 +31,72 @@ class ContinentalTest {
     void testCanLook() {
     	assertFalse(continental.canLook());
     }
+    
+    @Test
+    void testToStringLine1() {
+    	/*Door north not break*/
+    	assertEquals(continental.toString(0),"-----");
+    	/*Door north  break*/
+    	continental.getDoor(Direction.North).Break();
+    	assertEquals(continental.toString(0),"     ");
+    }
+    
+    
+
+    void testToStringLine2SomeBodyInCell() {
+    	continental.addZombies(zombie);
+    	/*Door north not break*/
+    	assertEquals(continental.toString(1),"|"+"\u001B[32m" +"C"+ "\u001B[0m"+"z1|");
+    	/*Door north  break*/
+    	continental.getDoor(Direction.East).Break();
+    	continental.getDoor(Direction.West).Break();
+    	assertEquals(continental.toString(1)," "+"\u001B[32m" +"C"+ "\u001B[0m"+"z1 ");
+    }
+
+	
+    	
+    @Test
+    void testToStringLine2NoBodyInCell() {	
+    	/*Door north not break*/
+    	assertEquals(continental.toString(1),"|"+"\u001B[32m" +"C"+ "\u001B[0m"+"  |");
+    	/*Door north  break*/
+    	continental.getDoor(Direction.East).Break();
+    	continental.getDoor(Direction.West).Break();
+    	assertEquals(continental.toString(1)," "+"\u001B[32m" +"C"+ "\u001B[0m"+"   ");
+    
+    }
+    
+    
+    
+    void testToStringLine3SomeBodyInCell() {
+    	continental.addZombies(zombie);
+    	/*Door north not break*/
+    	assertEquals(continental.toString(2),"|s1 |");
+    	/*Door north  break*/
+    	continental.getDoor(Direction.East).Break();
+    	continental.getDoor(Direction.West).Break();
+    	assertEquals(continental.toString(2)," s1  ");
+    }
+    	
+    @Test
+    void testToStringLine3NoBodyInCell() {	
+    	/*Door north not break*/
+    	assertEquals(continental.toString(2),"|   |");
+    	/*Door north  break*/
+    	continental.getDoor(Direction.East).Break();
+    	continental.getDoor(Direction.West).Break();
+    	assertEquals(continental.toString(2),"     ");
+    
+    }
+    
+    @Test
+    void testToStringLine4() {
+    	/*Door north not break*/
+    	assertEquals(continental.toString(3),"-----");
+    	/*Door north  break*/
+    	continental.getDoor(Direction.South).Break();
+    	assertEquals(continental.toString(3),"     ");
+    }
+    
 
 }
