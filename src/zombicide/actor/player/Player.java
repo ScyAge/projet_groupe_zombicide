22a0f3a7b5602418f5072a6900b5bc881b82ccbc
Joly <evan.joly.etu@univ-lille.fr>
2024-02-16 14:05:49 +0100

    ajout de la liste roles dans player ,mise en place de la methode actionSpécial ajout doc roles actionSpécial.

--- src/zombicide/actor/player/Player.java
+++ src/zombicide/actor/player/Player.java
@@ -8,12 +8,12 @@ import zombicide.item.*;
 
 
 
-
 public class Player extends Actor{
 	
 	private Map<String,Item>  Bagpack;
 	private  Item ItemInHand;
 	private int expertiseLevel;
+	private List<Roles> roles;
 	
 	public Player(int lifePoints) {
 		super(lifePoints,3);
@@ -55,6 +55,27 @@ public class Player extends Actor{
 
 	public Item getItemInHand() {
 		return this.ItemInHand;
+	}
+
+
+	/**
+	 * method that allows survivor to perform a SpecialAction related to its roles. If he has no role,
+	 * he does nothing; if he has only one, he executes the action; if he has more than one,
+	 * the role_number parameter lets him choose which action to use.
+	 * @param role_number number represent which of is roles the survivor want to use
+	 * @throws IndexOutOfBoundsException if role_number is in the range 0,size of the list roles
+	 */
+	public void actionSpecial(int role_number) throws IndexOutOfBoundsException{
+
+		int size = this.roles.size();
+		if(size ==1){
+			this.roles.get(0).action(this);
+		} else if (size >1) {
+			if((role_number > size)||(role_number < 0)){
+				throw new IndexOutOfBoundsException("choose a role number between 0 and"+(size-1));
+			}
+			this.roles.get(role_number).action(this);
+		}
 	}
 
 }
--- src/zombicide/actor/player/Roles.java
+++ src/zombicide/actor/player/Roles.java
@@ -1,6 +1,10 @@
 package zombicide.actor.player;
 
 public interface Roles {
+    /**
+     * method that allows a survivor to perform the special action related to its roles
+     * @param p the survivor who use the action
+     */
     public void action(Player p);
 }
 
