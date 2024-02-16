7433e3350ee105fa9f18311b13fddd6e5abfb780
Wissal Asma Harrat <wissalasma.harrat.etu@univ-lille.fr>
2024-02-16 14:29:04 +0100

    ajout de la Methode getZombiesInArea et des documentations

--- src/zombicide/actor/zombie/Zombies.java
+++ src/zombicide/actor/zombie/Zombies.java
@@ -1,29 +1,55 @@
 package zombicide.actor.zombie;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import zombicide.actor.*;
+import zombicide.cell.*;
 
 public  class Zombies extends Actor {
 	
-	protected int dommagePoints;
+	protected int damagePoints;
 	
 	
-	public Zombies(int lifePoints,int dommagePoints,int ActionPoints) {
-		super(lifePoints, ActionPoints);
-		this.dommagePoints = dommagePoints ;
+	public Zombies(int lifePoints,int action_points, Cell cell,int damagePoints) {
+		super(lifePoints, action_points, cell);
+		this.damagePoints = damagePoints ;
 	}
 
-	
-	public int getDommagePoints() {
-		return dommagePoints;
+	/**
+	 * gives the damagepoints of the zombie
+	 * @return damagepoints
+	 * */
+	public int getDamagePoints() {
+		return damagePoints;
 	}
-
-	public void setDommagePoints(int dommagePoints) {
-		this.dommagePoints = dommagePoints;
+	
+	/**
+	 * set the damage points
+	 * @param damagepoints*/
+	public void setDamagePoints(int damagePoints) {
+		this.damagePoints = damagePoints;
 	}
 	
+	/**
+	 * gives an information about how many damage points the zombie has
+	 * @return damage points
+	 * */
 	public String toString() {
-		return "Zombie have " + this.dommagePoints+"dommage Points";
+		return "Zombie have " + this.damagePoints+"damage Points";
 	}
 	
+	/** gives the list of zombies located in the same cell
+	 * @param the cell where the zombies are
+	 * @return players in the same cell
+	 * */
+	public List<Zombies> getZombiesInArea(Cell cell){
+		List<Zombies> zombies= new ArrayList<>();
+		
+		if(this.getCurrentCell()==cell) {
+			zombies.add(this);
+		}
+		return zombies;
+	}
 	
 }
