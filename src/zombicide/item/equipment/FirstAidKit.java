c4da2146d8dbef56330a5756099be0228fc2e81a
Wissal Asma Harrat <wissalasma.harrat.etu@univ-lille.fr>
2024-02-17 00:06:05 +0100

    Correction de la methode equipmentEffect(player)

--- src/zombicide/item/equipment/FirstAidKit.java
+++ src/zombicide/item/equipment/FirstAidKit.java
@@ -1,27 +1,41 @@
 package zombicide.item.equipment;
 
-import zombicide.item.*;
-import zombicide.player.*;
-import zombicide.actor.*;
+import java.util.List;
+import java.util.Scanner;
+
+import zombicide.actor.player.*;
+import zombicide.cell.*;
+
 
 
 public class FirstAidKit extends Equipment {
 	
-	public FirstAidKit(title: String) {
+	public FirstAidKit( String title) {
 		super(title);
 	}
 	
 	/**
-	 * heals the target player in the same area as the player 
+	 * heals a player in the same area as the player  
 	 * who has the FirstAidKit
 	 * @param player who has the FirstAidKit
-	 * @param the target who'll be healed
 	 * */
-	public void equipmentEffect(Player player, Player target) {
-		if(target!=null){
-			if( target.getCurrentCell()==player.getCurrentCell()) {
-				target.heal(1);
+	public void equipmentEffect(Player player ) {
+		Cell cell= player.getCurrentCell();
+		List<Player> players= player.getPlayersInArea(cell);
+		if(!players.isEmpty()) {
+			System.out.println("Enter the number of the player to heal");
+			Scanner scanner= new Scanner(System.in);
+			int index= scanner.nextInt();
+			if(index>0 && index<=players.size()) {
+				Player playerToHeal= players.get(index-1);
+				playerToHeal.heal(1);
+			}else {
+				System.out.println("invalid number");
 			}
+			
+		}else {
+			System.out.println("No player in the same Area");
 		}
+		
 	}
 }
