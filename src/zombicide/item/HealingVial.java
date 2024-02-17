5ade3a23c1b34a3137ce3d1c0385d0a097b99bdc
Wissal Asma Harrat <wissalasma.harrat.etu@univ-lille.fr>
2024-02-17 03:06:22 +0100

    ajout de méthode equipmentEffect

--- src/zombicide/item/HealingVial.java
+++ src/zombicide/item/HealingVial.java
@@ -1,10 +1,18 @@
 package zombicide.item;
 
+import zombicide.actor.player.Player;
+
 public class HealingVial extends Item {
 
 	public HealingVial(String title) {
 		super(title);
 	}
-
+	
+	/**
+	 * */
+	public void equipmentEfffect(Player player) {
+		int lifepoints= player.getLifePoints();
+		player.setLifePoints(lifepoints+1);
+	}
 
 }
