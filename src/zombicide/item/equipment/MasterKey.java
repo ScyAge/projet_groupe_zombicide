aa5d5fe06c63b7a28227776e83b15f0db5d02f39
Wissal Asma Harrat <wissalasma.harrat.etu@univ-lille.fr>
2024-02-17 03:33:17 +0100

    ajout de methode equipmentEffect(player) de Master key

--- src/zombicide/item/equipment/MasterKey.java
+++ src/zombicide/item/equipment/MasterKey.java
@@ -1,7 +1,10 @@
 package zombicide.item.equipment;
 
 import zombicide.actor.player.*;
-
+import zombicide.cell.Cell;
+import zombicide.util.Direction;
+import zombicide.util.Door;
+import java.util.*;
 
 
 public class MasterKey extends Equipment {
@@ -10,9 +13,21 @@ public class MasterKey extends Equipment {
 		super(title);
 	}
 	
+	/**
+	 * Opens the cell's door where the player is according to the user's choice
+	 * @param: player who has the MasterKey
+	 * */
 	public void equipmentEffect(Player player) {
-
+		System.out.println(" Enter the direction of the door you want to open");
+		System.out.println("choose between North, South, East, West:");
+		Scanner scanner= new Scanner(System.in);
+		String d = scanner.nextLine();
 		
+		Direction direction= Direction.valueOf(d);
+		Cell cell= player.getCurrentCell();
+		Door doorToOpen= cell.getDoor(direction);
+		doorToOpen.Break();
+			
 	}
 
 }
