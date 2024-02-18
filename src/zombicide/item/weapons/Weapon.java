07abb8c830432c5524ef00bdcf3316b6a5b7968a
Wissal Asma Harrat <wissalasma.harrat.etu@univ-lille.fr>
2024-02-18 23:10:02 +0100

    ajout de la javaDoc

--- src/zombicide/item/weapons/Weapon.java
+++ src/zombicide/item/weapons/Weapon.java
@@ -8,28 +8,46 @@ public class Weapon extends Item {
 	private WeaponType type;
 	private int threshold;
 	private int range;
-	private int domage;
+	private int damage;
 	
-	public Weapon(String title,int range,int domage, int threshold,WeaponType type) {
+	public Weapon(String title,int range,int damage, int threshold,WeaponType type) {
 		super(title);
-		this.domage =domage;
+		this.damage =damage;
 		this.range =range;
 		this.threshold = threshold;
 		this.type =type;
 	}
 	
+	
+	/**returns the threshold of the Weapon 
+	 * @return threshold of the given weapon
+	 * */
 	public int getThreshold() {
 		return this.threshold;
 	}
 
+	
+	/**gives the range of the given weapon
+	 * @return range of the weapon 
+	 * */
 	public int getRange() {
 		return this.range;
 	}
 
-	public int getDomage() {
-		return this.domage;
+	
+	/**
+	 * gives the damage caused by the given weapon 
+	 * @return damage 
+	 * */
+	public int getDamage() {
+		return this.damage;
 	}
 
+	
+	/**
+	 * gives the type of the weapon 
+	 * @return weapon's type 
+	 * */
 	public WeaponType getType() {
 		return type;
 	}
