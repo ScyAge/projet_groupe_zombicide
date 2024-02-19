9676c53f25737ddfe18b337520e704568a85c3d6
Wissal Asma Harrat <wissalasma.harrat.etu@univ-lille.fr>
2024-02-19 09:20:16 +0100

    ajout de la méthode createWeapon

--- src/zombicide/item/weapons/WeaponType.java
+++ src/zombicide/item/weapons/WeaponType.java
@@ -6,4 +6,31 @@ public enum WeaponType {
 	CHAINSAW,
 	GUN,
 	CARABINE;
+	
+	
+	/**
+	 * @param type of the weapon to initialize 
+	 * @return weapon 
+	 *  */
+	public Weapon createWeapon(WeaponType type) {
+		Weapon res = null;
+		for(WeaponType w :WeaponType.values()) {
+			if(w==WeaponType.CROWBAR) {
+				res= new Weapon("crowbar", 0, 1, 4, w);
+			}
+			else if (w==WeaponType.AXE) {
+				res= new Weapon("axe", 0, 2, 4, w);
+			}
+			else if(w==WeaponType.CHAINSAW) {
+				res= new Weapon("chainsaw", 0, 3, 5, w);
+			}
+			else if(w== WeaponType.GUN) {
+				res= new Weapon("gun", 1, 1, 4, w);
+			}
+			else {
+				res= new Weapon("carabine", 3, 1, 4, w) ;
+			}
+		}
+		return res;
+	}
 }
