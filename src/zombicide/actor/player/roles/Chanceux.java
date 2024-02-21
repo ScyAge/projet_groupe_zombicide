b43bf4a01bed989c0ee170378aa35ea1343dc07a
Joly <evan.joly.etu@univ-lille.fr>
2024-02-21 21:30:22 +0100

    ajout doc

--- src/zombicide/actor/player/roles/Chanceux.java
+++ src/zombicide/actor/player/roles/Chanceux.java
@@ -2,9 +2,12 @@ package zombicide.actor.player.roles;
 
 import zombicide.actor.player.Player;
 
+/**
+ * class for Chanceux
+ */
 public class Chanceux implements RolesIntrerface {
     @Override
     public void action(Player p) {
-
+    /*test2*/
     }
 }
