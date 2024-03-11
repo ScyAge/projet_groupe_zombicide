6c4226fe57a3fab9a0c742a453e193594fb1af96
Théo Nave <theonave59@gmail.com>
2024-03-11 23:18:07 +0100

    add item test

---
+++ test/zombicide/item/ItemTest.java
@@ -0,0 +1,27 @@
+package zombicide.item;
+import org.junit.jupiter.api.Test;
+import zombicide.actor.player.Player;
+import zombicide.cell.Room;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemTest {
+    @Test
+    void TestItemInitialization() {
+        Item item = new Item("test1");
+        assertEquals("test1", item.getTitle());
+        assertFalse(item.isUsed());
+    }
+
+    @Test
+    public void TestUseItem(){
+        Item item = new Item("test1");
+        Room room = new Room(5,5);
+        Player player = new Player(5,room,1);
+
+        assertFalse(item.isUsed());
+        item.Used();
+        assertTrue(item.isUsed());
+    }
+
+}
