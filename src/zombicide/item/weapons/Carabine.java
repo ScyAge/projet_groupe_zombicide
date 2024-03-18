670a40f3fa5f0895355ee87a3283298e3b9030ab
Théo Nave <theonave59@gmail.com>
2024-03-18 09:11:27 +0100

    add constant weapon : carabine

--- src/zombicide/item/weapons/Carabine.java
+++ src/zombicide/item/weapons/Carabine.java
@@ -1,14 +1,31 @@
 package zombicide.item.weapons;
 
+import javax.naming.Name;
+
 /**
  * class Gun
  */
 public class Carabine extends Weapon{
-
+    /**
+     * constant name of Carabine
+     */
+    private final static String Name_Carabine = "carabine";
+    /**
+     * constant range of Carabine
+     */
+    private final static int Range_Carabine = 3;
+    /**
+     * constant damage of Carabine
+     */
+    private final static int Damage_Carabine = 1;
+    /**
+     * constant threshold of Carabine
+     */
+    private final static int Threshold_Carabine = 4;
     /**
      * Builder of Carabine
      */
     public Carabine(){
-        super("carabine", 3, 1, 4, false);
+        super(Name_Carabine, Range_Carabine, Damage_Carabine, Threshold_Carabine, false);
     }
 }
