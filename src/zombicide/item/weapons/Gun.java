289349a08ea85c41a1c5c8864b74395895de7c85
Théo Nave <theonave59@gmail.com>
2024-03-18 09:12:34 +0100

    add constant weapon : gun

--- src/zombicide/item/weapons/Gun.java
+++ src/zombicide/item/weapons/Gun.java
@@ -4,11 +4,26 @@ package zombicide.item.weapons;
  * class Gun
  */
 public class Gun extends Weapon{
-
+    /**
+     * constant name of Gun
+     */
+    private final static String Name_Gun = "gun";
+    /**
+     * constant range of Gun
+     */
+    private final static int Range_Gun = 1;
+    /**
+     * constant damage of Gun
+     */
+    private final static int Damage_Gun = 1;
+    /**
+     * constant threshold of Gun
+     */
+    private final static int Threshold_Gun = 4;
     /**
      * Builder of Gun
      */
     public Gun(){
-        super("gun", 1, 1, 4, false);
+        super(Name_Gun, Range_Gun, Damage_Gun, Threshold_Gun, false);
     }
 }
