f47f97f441293f57b8cc14df82430576574abdd7
Théo Nave <theonave59@gmail.com>
2024-03-18 09:11:48 +0100

    add constant weapon : chainsaw

--- src/zombicide/item/weapons/Chainsaw.java
+++ src/zombicide/item/weapons/Chainsaw.java
@@ -4,11 +4,26 @@ package zombicide.item.weapons;
  * class Chainsaw
  */
 public class Chainsaw extends Weapon{
-
+    /**
+     * constant name of Chainsaw
+     */
+    private final static String Name_Chainsaw = "chainsaw";
+    /**
+     * constant range of Chainsaw
+     */
+    private final static int Range_Chainsaw = 0;
+    /**
+     * constant damage of Chainsaw
+     */
+    private final static int Damage_Chainsaw = 3;
+    /**
+     * constant threshold of Chainsaw
+     */
+    private final static int Threshold_Chainsaw = 5;
     /**
      * Builder of Chainsaw
      */
     public Chainsaw(){
-        super("chainsaw", 0, 3, 5, true);
+        super(Name_Chainsaw, Range_Chainsaw, Damage_Chainsaw, Threshold_Chainsaw, true);
     }
 }
