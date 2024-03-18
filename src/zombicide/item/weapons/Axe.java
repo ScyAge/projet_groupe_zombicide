6c5e9a6a25e0d11a804a17f3236b2296048ed5bd
Théo Nave <theonave59@gmail.com>
2024-03-18 09:10:46 +0100

    add constant weapon : axe

--- src/zombicide/item/weapons/Axe.java
+++ src/zombicide/item/weapons/Axe.java
@@ -4,11 +4,26 @@ package zombicide.item.weapons;
  * class Axe
  */
 public class Axe extends Weapon{
-
+    /**
+     * constant name of axe
+     */
+    private final static String Name_Axe = "axe";
+    /**
+     * constant range of axe
+     */
+    private final static int Range_Axe = 0;
+    /**
+     * constant damage of axe
+     */
+    private final static int Damage_Axe = 2;
+    /**
+     * constant threshold of axe
+     */
+    private final static int Threshold_Axe = 4;
     /**
      * Builder of Axe
      */
     public Axe(){
-        super("axe", 0, 2, 4, true);
+        super(Name_Axe, Range_Axe, Damage_Axe, Threshold_Axe, true);
     }
 }
