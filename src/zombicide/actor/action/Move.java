3008fa72e0149df930c99963c6f699f12c725574
Gabriel <gabriel.kreutser@gmail.com>
2024-03-22 18:21:15 +0100

    update move with list chooser

--- src/zombicide/actor/action/Move.java
+++ src/zombicide/actor/action/Move.java
@@ -1,27 +1,32 @@
 package zombicide.actor.action;
 
+import java.util.List;
+
 import zombicide.actor.player.Player;
 import zombicide.board.Board;
 import zombicide.util.Direction;
+import zombicide.util.listchooser.InteractiveListChooser;
+import zombicide.util.listchooser.ListChooser;
 
 /**
  * Class Move
  */
 public class Move implements Actions {
 	
-	private Direction direction;
-	
 	/**
 	 * Builder of Move
 	 */
-	public Move(Direction d) {
-		this.direction =d;
+	public Move() {
 	}
 	
 	
 	
 	public void action(Player p, Board b) {
-		b.movePlayer(p, this.direction);
+		System.out.println("enter the Direction to move");
+		List<Direction> directions= List.of(Direction.North, Direction.South, Direction.East, Direction.West);
+		ListChooser<Direction> listChooser = new InteractiveListChooser<>();
+		Direction D= listChooser.choose("choose a direction: ", directions);
+		b.movePlayer(p, D);
 	}
 }
 
