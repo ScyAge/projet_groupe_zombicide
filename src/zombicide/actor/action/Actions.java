5c8ea05f1ee9380d33de36ee5be853827a5d1320
Wissal Asma Harrat <wissalasma.harrat.etu@univ-lille.fr>
2024-03-22 22:04:55 +0100

    création de l'interface Actions

--- src/zombicide/actor/action/Actions.java
+++ src/zombicide/actor/action/Actions.java
@@ -1,10 +1,13 @@
 package zombicide.actor.action;
 
 import zombicide.actor.player.Player;
+import zombicide.actor.zombie.Zombies;
 import zombicide.board.Board;
 
 public interface Actions {
 	
 	
 	public void action(Player p, Board b);
+	
+	public void action(Zombies z, Board b);
 }
