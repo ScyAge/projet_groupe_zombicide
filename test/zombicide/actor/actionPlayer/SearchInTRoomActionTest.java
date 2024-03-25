0d7571beac8fa70abd9c2e880512a78ddaf5d021
joly <evan.joly.etu@univ-lille.fr>
2024-03-25 10:00:54 +0100

    test action

---
+++ test/zombicide/actor/actionPlayer/SearchInTRoomActionTest.java
@@ -0,0 +1,7 @@
+package zombicide.actor.actionPlayer;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+public class SearchInTRoomActionTest {
+
+}
--- test/zombicide/actor/actionPlayer/TakeInHandActionTest.java
+++ test/zombicide/actor/actionPlayer/TakeInHandActionTest.java
@@ -4,9 +4,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import zombicide.actor.player.Player;
 import zombicide.cell.*;
+import zombicide.item.Item;
 import zombicide.item.weapons.Axe;
 import zombicide.item.weapons.Carabine;
+import zombicide.item.weapons.Chainsaw;
 import zombicide.util.listchooser.RandomListChooser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TakeInHandActionTest {
     private ActionsPlayer action;
@@ -24,12 +29,26 @@ public class TakeInHandActionTest {
     }
 
     @Test
-    public void TestTakeInHandAction(){
+    public void TestTakeInHandActionNoItemInHand(){
         assertNull(this.p.getItemInHand());
         this.action.action(this.p);
         assertNotNull(this.p.getItemInHand());
         //before backpack size was 2
         assertEquals(1, this.p.getBackPack().size());
+    }
+
+    @Test
+    public void TestTakeInHandActionItemInHand(){
+        Item i = new Chainsaw();
+        this.p.setItemInHand(i);
+        List<Item> test= new ArrayList<>();
+        test.add(i);
+        test.addAll(this.p.getBackPack());
+        assertNotNull(this.p.getItemInHand());
+        this.action.action(this.p);
+        assertTrue(this.p.getBackPack().contains(i));
+        assertEquals(2, this.p.getBackPack().size());
+        assertNotNull(this.p.getItemInHand());
     }
 
 }
