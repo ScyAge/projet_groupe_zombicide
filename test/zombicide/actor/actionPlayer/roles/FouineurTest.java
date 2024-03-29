6b73d7432c1cd401e68ab6ed1da4dba3896da27d
Joly <evan.joly.etu@univ-lille.fr>
2024-03-29 15:24:05 +0100

    test et finalisation role Fouineur.java

---
+++ test/zombicide/actor/actionPlayer/roles/FouineurTest.java
@@ -0,0 +1,52 @@
+package zombicide.actor.actionPlayer.roles;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import zombicide.actor.actionPlayer.ActionsPlayer;
+import zombicide.actor.actionPlayer.SearchInTRoomAction;
+import zombicide.actor.player.Player;
+import zombicide.board.TrainingBoard;
+import zombicide.cell.Cell;
+import zombicide.item.weapons.Axe;
+import zombicide.item.weapons.Carabine;
+import zombicide.item.weapons.Chainsaw;
+import zombicide.util.listchooser.ListChooser;
+import zombicide.item.*;
+import zombicide.util.listchooser.RandomListChooser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FouineurTest {
+    private ActionsPlayer act;
+    private ListChooser<Item> chooser;
+
+    private Player p;
+    private TrainingBoard board;
+    private Cell cell;
+    private Item wp1;
+    private Item wp2;
+    @BeforeEach
+    public void init(){
+        this.chooser = new RandomListChooser<>();
+        this.board = new TrainingBoard();
+        this.cell = this.board.getCellBoard(0,0);
+        this.p = new Player(3,this.cell,1,5);
+        this.cell.addPlayers(this.p);
+        this.act = new Fouineur(this.chooser);
+        this.wp1 = new Carabine(this.board);
+        this.wp2 = new Chainsaw(this.board);
+        this.cell.addItem(this.wp1);
+        this.cell.addItem(this.wp2);
+        this.p.putItemInBackPack(new Carabine(board));
+        this.p.putItemInBackPack(new Axe(board));
+    }
+
+    @Test
+    public void TestActionFouineur(){
+        int acp = this.p.getAction_points();
+        this.act.action(this.p);
+        assertEquals(this.p.getAction_points(), acp);
+    }
+
+}
