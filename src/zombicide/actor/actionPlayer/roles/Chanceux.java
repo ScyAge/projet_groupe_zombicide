e8a9469f8d5db6165431a9523809f537d3d6e56e
Wissal Asma Harrat <wissalasma.harrat.etu@univ-lille.fr>
2024-03-30 04:22:01 +0100

    ajout la méthode privée attackZombie et action(player)

--- src/zombicide/actor/actionPlayer/roles/Chanceux.java
+++ src/zombicide/actor/actionPlayer/roles/Chanceux.java
@@ -1,7 +1,14 @@
 package zombicide.actor.actionPlayer.roles;
 
+import java.util.Random;
+
 import zombicide.actor.actionPlayer.ActionsPlayer;
 import zombicide.actor.player.Player;
+import zombicide.actor.zombie.Zombies;
+import zombicide.cell.Cell;
+import zombicide.item.weapons.Weapon;
+import zombicide.util.listchooser.InteractiveListChooser;
+import zombicide.util.listchooser.ListChooser;
 
 /**
  * class for Chanceux
@@ -9,10 +16,24 @@ import zombicide.actor.player.Player;
 public class Chanceux implements ActionsPlayer {
 	
 	/**
+	 * param 
+	 */
+	private ListChooser<Zombies> chooser;
+	
+	/**
+	 * Builder of Chanceux with ListChooser in param to test
+	 * @param chooser the listchooser of the action
+	 */
+	public Chanceux(ListChooser<Zombies> chooser) {
+		this.chooser = chooser;
+	}
+	
+	
+	/**
 	 * Builder of Chanceux
 	 */
 	public Chanceux() {
-		
+		this.chooser = new InteractiveListChooser<>();
 	}
 	
     /**
@@ -21,6 +42,33 @@ public class Chanceux implements ActionsPlayer {
      */
     @Override
     public void action(Player p) {
-    /*test*/
+    	Cell c= p.getCurrentCell();
+		Zombies z= chooser.choose("pick a zombie to attack : ", c.getAllZombies());
+		attackZombie(p,z);
     }
+    
+ 
+    /**
+	 * attacks the zombie by the given player and adds a dice 
+	 * @param p player that's going to attack
+	 * @param z the zombie that's being attacked
+	 * */
+	private void attackZombie(Player p, Zombies z) {
+		Weapon w= (Weapon) p.getItemInHand();
+		
+		Random random= new Random();
+		for(int i=0; i<w.getNbDice()+1; i++) {
+			int dice= random.nextInt(6)+1;
+			
+			if(dice>=w.getThreshold()) {
+				z.takeDamage(w.getDamage());
+				
+				if(z.isDead()) {
+					p.UpOneExpertiseLevel();
+				}
+			}else {
+				System.out.println("attack failed");
+			}
+		}
+	}
 }
--- src/zombicide/actor/actionPlayer/roles/Combattant.java
+++ src/zombicide/actor/actionPlayer/roles/Combattant.java
@@ -1,7 +1,14 @@
 package zombicide.actor.actionPlayer.roles;
 
+import java.util.Random;
+
 import zombicide.actor.actionPlayer.ActionsPlayer;
 import zombicide.actor.player.Player;
+import zombicide.actor.zombie.Zombies;
+import zombicide.cell.Cell;
+import zombicide.item.weapons.Weapon;
+import zombicide.util.listchooser.InteractiveListChooser;
+import zombicide.util.listchooser.ListChooser;
 
 /**
  * Class Combattant
@@ -9,10 +16,24 @@ import zombicide.actor.player.Player;
 public class Combattant implements ActionsPlayer {
 	
 	/**
+	 * param 
+	 */
+	private ListChooser<Zombies> chooser;
+	
+	/**
+	 * Builder of Combattant with ListChooser in param to test
+	 * @param chooser the listchooser of the action
+	 */
+	public Combattant(ListChooser<Zombies> chooser) {
+		this.chooser = chooser;
+	}
+	
+	
+	/**
 	 * Builder of Combattant
 	 */
 	public Combattant() {
-		
+		this.chooser = new InteractiveListChooser<>();
 	}
 	
     /**
@@ -21,6 +42,32 @@ public class Combattant implements ActionsPlayer {
      */
     @Override
     public void action(Player p) {
-
+    	Cell c= p.getCurrentCell();
+		Zombies z= chooser.choose("pick a zombie to attack : ", c.getAllZombies());
+		attackZombie(p,z);
     }
+    /**
+	 * attacks the zombie by the given player and adds a point to the Dice result
+	 * takes a point from the weapon's threashold
+	 * @param p player that's going to attack
+	 * @param z the zombie that's being attacked
+	 * */
+	private void attackZombie(Player p, Zombies z) {
+		Weapon w= (Weapon) p.getItemInHand();
+		
+		Random random= new Random();
+		for(int i=0; i<w.getNbDice(); i++) {
+			int dice= random.nextInt(6)+1;
+			
+			if(dice>=w.getThreshold()-1) {
+				z.takeDamage(w.getDamage());
+				
+				if(z.isDead()) {
+					p.UpOneExpertiseLevel();
+				}
+			}else {
+				System.out.println("attack failed");
+			}
+		}
+	}
 }
