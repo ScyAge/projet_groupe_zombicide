a480d6920b4e74c827e2ba98642ffafd596e596a
Wissal Asma Harrat <wissalasma.harrat.etu@univ-lille.fr>
2024-03-30 04:07:11 +0100

    ajout d'une méthode attackZombie

--- src/zombicide/actor/actionPlayer/Attack.java
+++ src/zombicide/actor/actionPlayer/Attack.java
@@ -1,10 +1,15 @@
 package zombicide.actor.actionPlayer;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import zombicide.actor.player.Player;
 import zombicide.actor.zombie.Zombies;
 import zombicide.board.Board;
+import zombicide.cell.Cell;
+import zombicide.item.Item;
+import zombicide.item.weapons.Weapon;
 import zombicide.util.Direction;
 import zombicide.util.listchooser.InteractiveListChooser;
 import zombicide.util.listchooser.ListChooser;
@@ -14,13 +19,13 @@ public class Attack implements ActionsPlayer{
 	/**
 	 * param 
 	 */
-	private ListChooser<Direction> chooser;
+	private ListChooser<Zombies> chooser;
 	
 	/**
 	 * Builder of Attack with ListChooser in param to test
 	 * @param chooser the listchooser of the action
 	 */
-	public Attack(ListChooser<Direction> chooser) {
+	public Attack(ListChooser<Zombies> chooser) {
 		this.chooser = chooser;
 	}
 	
@@ -38,8 +43,44 @@ public class Attack implements ActionsPlayer{
 	 * @param p player who attack a zombie
 	 */
 	public void action(Player p) {
-		if(p.getItemInHand()!=null) {
-			p.getItemInHand().ItemEffect(p);
+		Cell c= p.getCurrentCell();
+		Zombies z= chooser.choose("pick a zombie to attack : ", c.getAllZombies());
+		attackZombie(p,z);
+	}
+	
+	/*private List<Zombies> zombiesInTheCell(Cell c) {
+		List<Zombies> zombiesId= new ArrayList<>();
+ 		if( c.getAllZombies()!=null) {
+			for(Zombies z: c.getAllZombies()) {
+				zombiesId.add(z.getId());
+			}
+		}
+		return zombiesId;
+	}
+	*/
+	
+	/**
+	 * attacks the zombie by the given player
+	 * @param p player that's going to attack
+	 * @param z the zombie that's being attacked
+	 * */
+	private void attackZombie(Player p, Zombies z) {
+		Weapon w= (Weapon) p.getItemInHand();
+		
+		Random random= new Random();
+		for(int i=0; i<w.getNbDice(); i++) {
+			int dice= random.nextInt(6)+1;
+			
+			if(dice>=w.getThreshold()) {
+				z.takeDamage(w.getDamage());
+				
+				if(z.isDead()) {
+					p.UpOneExpertiseLevel();
+				}
+			}else {
+				System.out.println("attack failed");
+			}
 		}
 	}
+	
 }
