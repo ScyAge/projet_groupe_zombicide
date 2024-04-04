3d5da48d4d0f59675a909ec49aa162c6ef0a410a
Wissal Asma Harrat <wissalasma.harrat.etu@univ-lille.fr>
2024-04-04 08:30:53 +0200

    ajout d'une méthode display(Player)

--- src/zombicide/actor/actionPlayer/LookAround.java
+++ src/zombicide/actor/actionPlayer/LookAround.java
@@ -22,32 +22,39 @@ public class LookAround implements ActionsPlayer {
 
 	@Override
 	public void action(Player p) {
+		String res= display(p);
+		System.out.println(res);
+	}
+	
+	public String display(Player p) {
 		Cell c= p.getCurrentCell();
 		System.out.println("Description of the area : "+ c.description());
 		
 		List<Player> players= c.getAllPlayers();
 		List<Zombies> zombies= c.getAllZombies();
+		String res="";
 		
 		if(!players.isEmpty()) {
-			System.out.println("Players in this Area : ");
+			res+="Players in this Area : ";
 			for(Player player: players) {
-				System.out.println(player.getId());
+				res+=player.getId();
 			}
 		}
 		if (!zombies.isEmpty()) {
-			System.out.println("Zombies in this Area : ");
+			res+="Zombies in this Area : ";
 			for(Zombies z: zombies) {
-				System.out.println(z.getId());
+				res+=z.getId();
 			}
 		}
 		
-		System.out.println("The opened doors : ");
+		res+="The opened doors : ";
 		for(Direction d: Direction.values()) {
 			Door door= c.getDoor(d);
 			if(door.isBreak()) {
-				System.out.println("the door at the direction "+ d + "is opened");
+				res+="the door at the direction "+ d + "is opened";
 			}
 		}
+		return res;
 	}
 
 	
