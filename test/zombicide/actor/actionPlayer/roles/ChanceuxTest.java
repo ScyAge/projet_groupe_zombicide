681325cdf50782bf5965e963ee8c2e63e8b3b771
Wissal Asma Harrat <wissalasma.harrat.etu@univ-lille.fr>
2024-04-08 09:39:15 +0200

    réalisation des tests de Chanceux

---
+++ test/zombicide/actor/actionPlayer/roles/ChanceuxTest.java
@@ -0,0 +1,74 @@
+package zombicide.actor.actionPlayer.roles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import zombicide.actor.player.Player;
+import zombicide.actor.zombie.Broom;
+
+import zombicide.actor.zombie.Zombies;
+import zombicide.board.TrainingBoard;
+import zombicide.cell.Cell;
+import zombicide.item.Item;
+
+import zombicide.item.weapons.Weapon;
+import zombicide.util.listchooser.ListChooser;
+import zombicide.util.listchooser.RandomListChooser;
+
+public class ChanceuxTest {
+	
+	private Player player;
+	private Zombies z1;
+	private Zombies z2;
+	private Chanceux action;
+    private ListChooser<Zombies> chooser;
+    private TrainingBoard board;
+    private Cell cell;
+    private Item wp1;
+    private Item wp2;
+    
+    @BeforeEach
+	public void init() {
+		 this.chooser = new RandomListChooser<>();
+		 this.board = new TrainingBoard();
+	     this.cell = this.board.getCellBoard(1,1);
+	     this.player = new Player(3,this.cell,1,5);
+	  
+	     this.cell.addPlayers(this.player);
+	     this.action= new Chanceux(this.chooser, this.board );
+	     this.wp1 = new Weapon("Axe",0,0,2,0,false,true,1);
+	     this.wp2 = new Weapon("gun",4,0,1,0,false,true,1);
+	     this.z1= new Broom(this.cell, 1);
+	     this.z2= new Broom(this.cell, 2);
+		 this.cell.addZombies(z1);
+		 this.cell.addZombies(z2);
+	}
+	
+    @Test
+    public void ActionTestWithAxe() {
+    	this.player.setItemInHand(wp1);
+    	int LP1 = this.z1.getLifePoints();
+    	int LP2 = this.z2.getLifePoints();
+    	this.action.action(this.player);
+    	
+    	
+    	assertTrue(LP1-4== z1.getLifePoints() || LP2-4== z2.getLifePoints() || (LP1-2== z1.getLifePoints() && LP2-2==z2.getLifePoints()) );
+    	
+    	
+    }
+    
+    @Test 
+    public void ActionTestWithGun() {
+    	this.player.setItemInHand(wp2);
+    	int LP1 = this.z1.getLifePoints();
+    	int LP2 = this.z2.getLifePoints();
+    	this.action.action(this.player);
+    	
+    	System.out.println(z1.getLifePoints() +" and "+ z2.getLifePoints());
+    	assertEquals(z1.getLifePoints(), LP1 );
+    	assertEquals(z2.getLifePoints(), LP2);
+    	
+    }
+}
