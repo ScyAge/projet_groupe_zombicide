9b1886d0953c211851ccce6b977cf9513f834e52
Joly <evan.joly.etu@univ-lille.fr>
2024-04-11 23:00:31 +0200

    ADD Game.java TestGame.java -ajout d'un constructeur et de parametre pour game -ajout d'une méthode AreTheyAllAlive dans Game -Ajout du debut de la fonction plat -test de la methode AreTheyAllAlive

---
+++ src/zombicide/Game.java
@@ -0,0 +1,57 @@
+package zombicide;
+
+import zombicide.actor.actionPlayer.ActionsPlayer;
+import zombicide.actor.player.Player;
+import zombicide.board.Board;
+import zombicide.item.Item;
+import zombicide.util.listchooser.ListChooser;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Game {
+    private final List<Player> allPLayers;
+    private final Board board;
+    private final List<ActionsPlayer>AllActionPlayer;
+    private final List<Item> allItem;
+
+    private final ListChooser<ActionsPlayer> PlayerChooser;
+
+    public Game(Board b, List<Player> players, List<ActionsPlayer> AllActions, List<Item> AllItem,ListChooser<ActionsPlayer> PlayerChooser) {
+        this.board = b;
+        this.allPLayers = players;
+        this.AllActionPlayer = AllActions;
+        this.allItem = AllItem;
+        this.PlayerChooser = PlayerChooser;
+
+    }
+
+    public boolean AreTheyAllAlive(){
+        List<Boolean> test = new ArrayList<>();
+        for(Player p :  this.allPLayers){
+            test.add(p.isDead());
+        }
+        Stream<Boolean> res = test.stream().filter(c -> !c);
+        return  res.toList().contains(false);
+    }
+
+
+    public void play(){
+        while(this.AreTheyAllAlive()){
+            //Phase 1 tour des joueur
+            for(Player p : this.allPLayers){
+                while(p.getAction_points() > 0){
+                    List<ActionsPlayer> actionPossible = p.getActionOfThePlayer();
+                    ActionsPlayer actionHeChoose = this.PlayerChooser.choose("Qu'elle action souhaite tu faire",actionPossible);
+                    actionHeChoose.action(p);
+                }
+            }
+            //Action des Zombie
+
+        }
+    }
+
+
+}
---
+++ test/zombicide/TestGame.java
@@ -0,0 +1,77 @@
+package zombicide;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import zombicide.actor.actionPlayer.*;
+import zombicide.actor.player.Player;
+import zombicide.board.Board;
+import zombicide.board.TrainingBoard;
+import zombicide.util.listchooser.RandomListChooser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class TestGame {
+
+    private Game game;
+    private Player p1;
+    private Player p2;
+    private Player p3;
+    private Player p4;
+    private Board b;
+    @BeforeEach
+    public void init(){
+        this.b = new TrainingBoard();
+        this.p1= new Player(5, b.getCellBoard(0,0), 1, 6);
+        this.p2= new Player(5, b.getCellBoard(0,0), 2, 6);
+        this.p3 = new Player(5, b.getCellBoard(0,0), 3, 6);
+        this.p4 = new Player(5, b.getCellBoard(0,0), 4, 6);
+        List<Player> AllP = new ArrayList<>();
+        AllP.add(this.p1);
+        AllP.add(this.p2);
+        AllP.add(this.p3);
+        AllP.add(this.p4);
+
+        ActionsPlayer take = new TakeInHandAction(new RandomListChooser<>());
+        ActionsPlayer LA =new LookAround(b);
+        ActionsPlayer OD = new OpenDoor(new RandomListChooser<>(),b);
+        ActionsPlayer move = new Move(b,new RandomListChooser<>());
+        ActionsPlayer noise = new MakeNoise();
+        ActionsPlayer useEquip = new UseEquipmentAction();
+        ActionsPlayer attack = new Attack(new RandomListChooser<>(),b);
+        ActionsPlayer search = new SearchInTRoomAction(new RandomListChooser<>());
+        List<ActionsPlayer> actions = new ArrayList<>();
+        actions.add(take);
+        actions.add(LA);
+        actions.add(OD);
+        actions.add(move);
+        actions.add(noise);
+        actions.add(useEquip);
+        actions.add(attack);
+        actions.add(search);
+
+        this.game = new Game(this.b,AllP,actions,null,new RandomListChooser<>());
+    }
+
+
+    @Test
+    public void TestAreTheyAllAlive(){
+        //no player dead
+        assertTrue(this.game.AreTheyAllAlive());
+
+        //one dead
+        this.p1.takeDamage(10000);
+        assertTrue(this.p1.isDead());
+        assertTrue(this.game.AreTheyAllAlive());
+
+        //All dead
+        this.p2.takeDamage(10000);
+        this.p3.takeDamage(10000);
+        this.p4.takeDamage(10000);
+        assertTrue(this.p2.isDead());
+        assertTrue(this.p3.isDead());
+        assertTrue(this.p4.isDead());
+        assertFalse(this.game.AreTheyAllAlive());
+
+    }
+}
